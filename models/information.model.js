0797652f409efd2ff5ad1811ffd767bd7a502b06
noiredev <noiresama@gmail.com>
2022-12-07 12:02:16 +0000

    Information schema fixed

--- models/information.model.js
+++ models/information.model.js
@@ -1,15 +1,14 @@
 import mongoose, { Schema } from "mongoose";
 
 const informationSchema = new Schema({
-  profileid: { type: Number, required: true, unique: true },
-  badge: [
-    {
-      badgeName: String,
-      badgeDetails: String,
-    },
-  ],
-  scores: [{ scoreName: String, scoreNumber: Number }],
-  videoLink: { type: String, required: true, unique: true },
+	profileid: { type: Number, required: true, unique: true },
+	badge: [
+		{
+			badgeName: String,
+			badgeDetails: String,
+		}],
+	scores: [{ scoreName: String, scoreNumber: Number }],
+	videoLink: { type: String, required: true, unique: true },
 });
 
 const Information = mongoose.model(`Information`, informationSchema);
--- routes/addInformationRouter.js
+++ routes/addInformationRouter.js
@@ -15,31 +15,29 @@ export const router = express.Router();
 // }
 
 router.route(`/:profileid`).post(
-  [
-    check("videoLink").exists().trim().escape(),
-    check("badge").exists(),
-    check("badgeSchema").isLength({ min: 2, max: 2 }),
-    // .badgeSchemaChecker("add input"),
-    check("badgeSchema").isLength({ min: 2, max: 2 }),
-    // .scoreSchemaChecker("add input"),
-  ],
+	[
+		check("videoLink").exists().trim().escape(),
+		// check("badge").exists(),
+		// check("badgeSchema").isLength({ min: 2, max: 2 }),
+		// .badgeSchemaChecker("add input"),
+		// check("badgeSchema").isLength({ min: 2, max: 2 }),
+		// .scoreSchemaChecker("add input"),
+	],
 
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.log(errors);
-      return res.status(422).json({
-        message: `There is a Error in information data`,
-      });
-    }
-    const informationData = new Information(req.body);
-    try {
-      const information = await informationData.save();
-      res.status(201).json(information);
-    } catch {
-      res.status(400).json({
-        message: `Information Insertion Failed`,
-      });
-    }
-  }
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log(errors);
+			return res.status(422).json({
+				message: `There is an Error in the information data`,
+			});
+		}
+		const informationData = new Information(req.body);
+		try {
+			const information = await informationData.save();
+			res.status(201).json(information);
+		} catch (error) {
+			res.status(400).json({ message: error.message });
+		}
+	}
 );
