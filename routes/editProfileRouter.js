ac309c35396393cf5139d61bb736a145cfd22dd8
Oonagh Parker <oonaghparker@gmail.com>
2022-12-14 14:26:42 +0000

    edit profile added

--- routes/editProfileRouter.js
+++ routes/editProfileRouter.js
@@ -4,20 +4,19 @@ import Profiles from "../models/profile.model.js";
 export const router = express.Router();
 
 router
-    .route(`/`)
-    .put((req, res) => {
-        const id = req.body.profileid;
-        Profiles.findById(id, (error, profile) => {
-            if (!profile) {
-                res.status(404).send(`Profile could not be found`);
-            } else {
-                const updatedData = new Profiles(req.body);
-                const updatedProfile = Object.assign(profile, updatedData);
-                updatedProfile.save()
-                    .then(profile => {
-                        res.status(201).json({ profile: `Profile updated successfully` });
-                    })
-                    .catch(error => res.status(400).send({ error: 'Updating profile failed' }));
+    .route(`/:profileid`)
+    .put(async (req, res) => {
+        const profileid = req.params.profileid;
+        if (!profileid) {
+            res.status(404).send(`Profile could not be found`);
+        } else {
+            const updatedData = req.body;
+            try {
+                await Profiles.findOneAndUpdate({ profileid: profileid }, updatedData);
+                res.send(`Profile updated!`);
+            } catch (error) {
+                console.log(error);
+                res.status(404).send(`That profile cannot be found`)
             }
-        })
+        }
     });
